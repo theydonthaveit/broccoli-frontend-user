ffce857efb41b308df251a3a989f0a588817df89
Alan Williams <theydonthaveit@gmail.com>
2017-10-19 14:10:48 +0100

    TRELLO-1: App Intro: Initial work on the 3 screens

--- App.js
+++ App.js
@@ -1,95 +1,38 @@
-import React, { Component } from 'react';
-import { Button, Animated, View, StyleSheet, Image, Dimensions, ScrollView } from 'react-native'
+import React, { Component } from 'react'
+import { Text, Button, View, StyleSheet, Image, ScrollView, Dimensions } from 'react-native'
 
-const deviceWidth = Dimensions.get('window').width
-const FIXED_BAR_WIDTH = 280
-const BAR_SPACE = 10
+const { width } = Dimensions.get('window')
+const { height } = Dimensions.get('window')
 
-const images = [
-  'https://s-media-cache-ak0.pinimg.com/originals/ee/51/39/ee5139157407967591081ee04723259a.png',
-  'https://s-media-cache-ak0.pinimg.com/originals/40/4f/83/404f83e93175630e77bc29b3fe727cbe.jpg',
-  'https://s-media-cache-ak0.pinimg.com/originals/8d/1a/da/8d1adab145a2d606c85e339873b9bb0e.jpg',
-]
+class IntroImagesJoin extends Component {
+  render() {
+    return (
+      <Image style={styles.image} source={{uri: this.props.imageSrc}} />
+    )
+  }
+}
+
+class IntroImages extends Component {
+  render() {
+    return (
+      <Image style={styles.image} source={{uri: this.props.imageSrc}} /> 
+    )
+  }
+}
 
 export default class App extends Component {
-
-  numItems = images.length
-  itemWidth = (FIXED_BAR_WIDTH / this.numItems) - ((this.numItems - 1) * BAR_SPACE)
-  animVal = new Animated.Value(0)
-
   render() {
-    let imageArray = []
-    let barArray = []
-    images.forEach((image, i) => {
-      console.log(image, i)
-      const thisImage = (
-        <Image
-          key={`image${i}`}
-          source={{uri: image}}
-          style={{ width: deviceWidth }}
-        />
-      )
-      imageArray.push(thisImage)
-
-      const scrollBarVal = this.animVal.interpolate({
-        inputRange: [deviceWidth * (i - 1), deviceWidth * (i + 1)],
-        outputRange: [-this.itemWidth, this.itemWidth],
-        extrapolate: 'clamp',
-      })
-
-      const thisBar = (
-        <View
-          key={`bar${i}`}
-          style={[
-            styles.track,
-            {
-              width: this.itemWidth,
-              marginLeft: i === 0 ? 0 : BAR_SPACE,
-            },
-          ]}
-        >
-          <Animated.View
-
-            style={[
-              styles.bar,
-              {
-                width: this.itemWidth,
-                transform: [
-                  { translateX: scrollBarVal },
-                ],
-              },
-            ]}
-          />
-        </View>
-      )
-      barArray.push(thisBar)
-    })
-
     return (
-      <View
-        style={styles.container}
-        flex={1}
-      >
+      <View style={styles.container}>
         <ScrollView
           horizontal
-          showsHorizontalScrollIndicator={false}
-          scrollEventThrottle={10}
           pagingEnabled
-          onScroll={
-            Animated.event(
-              [{ nativeEvent: { contentOffset: { x: this.animVal } } }]
-            )
-          }
+          showsHorizontalScrollIndicator={false}
         >
-
-          {imageArray}
-
+          <IntroImages imageSrc='http://blog.truephotography.com/wp-content/uploads/2011/02/4be22064-0ec4-481a-9b5a-623143cfd45a-1-600x400.jpg' />
+          <IntroImages imageSrc='http://images.dailyhive.com/20160915073851/smoothie.jpg' />
+          <IntroImagesJoin imageSrc='http://s2.r29static.com//bin/entry/87b/406x487,80/1741652/image.jpg' />
         </ScrollView>
-        <View
-          style={styles.barContainer}
-        >
-          {barArray}
-        </View>
       </View>
     )
   }
@@ -101,23 +44,10 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    height
   },
-  barContainer: {
-    position: 'absolute',
-    zIndex: 2,
-    top: 40,
-    flexDirection: 'row',
-  },
-  track: {
-    backgroundColor: '#ccc',
-    overflow: 'hidden',
-    height: 2,
-  },
-  bar: {
-    backgroundColor: '#5294d6',
-    height: 2,
-    position: 'absolute',
-    left: 0,
-    top: 0,
-  },
+  image: {
+    width,
+    height
+  }
 })

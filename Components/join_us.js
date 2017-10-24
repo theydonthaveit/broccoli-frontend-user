a49965eb01c606c741a635f4927d8718ca89aaff
Alan Williams <theydonthaveit@gmail.com>
2017-10-24 13:19:42 +0100

    TRELLO-3: Prep work for TRELLO-4 ticket to add more fields and swipe to next form

--- Components/join_us.js
+++ Components/join_us.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { View, Text, Button } from 'react-native'
+import { View, Text, Button, ScrollView } from 'react-native'
 import { FormLabel, FormInput, FormValidationMessage } from 'react-native-elements'
 
 import styles from './styles'
@@ -73,45 +73,55 @@ export default class JoinUs extends Component {
 
     render() {
         return (
-        <View>
-            <FormLabel>first name</FormLabel>
-            <FormInput
-                value={this.state.firstname}
-                onChangeText={(firstname) => this._validate_first_name(firstname)}/>
-            <FormValidationMessage>{this.state.firstname === 'Mine?' ? 'Required' : ''}</FormValidationMessage>
-            <FormLabel>last name</FormLabel>
-            <FormInput
-                value={this.state.lastname}
-                onChangeText={(lastname) => this._validate_last_name(lastname)}/>
-            <FormValidationMessage>{this.state.lastname === 'You must have one' ? 'Required' : ''}</FormValidationMessage>
-            <FormLabel>email</FormLabel>
-            <FormInput
-                value={this.state.email}
-                onChangeText={(email) => this._set_email(email)}/>
-            <FormValidationMessage>
-                {this._validate_email(this.state.email)}
-            </FormValidationMessage>
-            <FormLabel>password</FormLabel>
-            <FormInput
-                value={this.state.password}
-                onChangeText={(password) => this._validate_password(password)}/>
-            <FormValidationMessage>{this.state.password === 'Be Cryptic' ? 'Required' : ''}</FormValidationMessage>
-            <FormLabel>repeat password</FormLabel>
-            <FormInput
-                value={this.state.repassword}
-                onChangeText={(repassword) => this._set_password(repassword)}/>
-            <FormValidationMessage>
-                { this._check_password(this.state.password) }
-            </FormValidationMessage>
-            <Button
-                onPress={this._submitForm}
-                title="Connect"
-                color="#000"
-            />
-            <FormValidationMessage>
-                { this._check_password(this.state.password) }
-            </FormValidationMessage>
-        </View>
+            <View>
+                <ScrollView
+                    horizontal
+                    pagingEnabled
+                    showsHorizontalScrollIndicator={false}
+                    >
+                    <View style={styles.intro.image}>
+                        <FormLabel>first name</FormLabel>
+                        <FormInput
+                            value={this.state.firstname}
+                            onChangeText={(firstname) => this._validate_first_name(firstname)}/>
+                        <FormValidationMessage>{this.state.firstname === 'Mine?' ? 'Required' : ''}</FormValidationMessage>
+                        <FormLabel>last name</FormLabel>
+                        <FormInput
+                            value={this.state.lastname}
+                            onChangeText={(lastname) => this._validate_last_name(lastname)}/>
+                        <FormValidationMessage>{this.state.lastname === 'You must have one' ? 'Required' : ''}</FormValidationMessage>
+                        <FormLabel>email</FormLabel>
+                        <FormInput
+                            value={this.state.email}
+                            onChangeText={(email) => this._set_email(email)}/>
+                        <FormValidationMessage>
+                            {this._validate_email(this.state.email)}
+                        </FormValidationMessage>
+                        <FormLabel>password</FormLabel>
+                        <FormInput
+                            value={this.state.password}
+                            onChangeText={(password) => this._validate_password(password)}/>
+                        <FormValidationMessage>{this.state.password === 'Be Cryptic' ? 'Required' : ''}</FormValidationMessage>
+                        <FormLabel>repeat password</FormLabel>
+                        <FormInput
+                            value={this.state.repassword}
+                            onChangeText={(repassword) => this._set_password(repassword)}/>
+                        <FormValidationMessage>
+                            { this._check_password(this.state.password) }
+                        </FormValidationMessage>
+                    </View>
+                    <View style={styles.intro.image}>
+                        <Button
+                            onPress={this._submitForm}
+                            title="Connect"
+                            color="#000"
+                        />
+                        <FormValidationMessage>
+                            { this._check_password(this.state.password) }
+                        </FormValidationMessage>
+                    </View>
+                </ScrollView>
+            </View>
         )
     }
 }
